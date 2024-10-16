bf65a352cbfab9453f33d046f39c0a0440d5e158
Gabriela Escalona <gescalona2022@udec.cl>
2024-10-16 17:46:09 -0300

    feat:Implementacion de metodos y gestion de retrasos

--- src/main/java/Retraso.java
+++ src/main/java/Retraso.java
@@ -1,5 +1,18 @@
+import java.time.Duration;
+import java.time.Instant;
+
 public class Retraso extends Asistencia{
-    public Retraso(){
+    private Duration tiempoRetraso;
+    public Retraso(Empleado empleado, Instant horaLlegada, Instant horaInicio){
+        super(empleado, true, horaLlegada);
+        this.tiempoRetraso = Duration.between(horaInicio, horaLlegada);
+    }
+    public Duration getTiempoRetraso(){
+        return tiempoRetraso;
+    }
+    public String toString(){
 
     }
 }
+
+
