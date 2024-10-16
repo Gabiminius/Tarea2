d64f29fa0d2035c457046ba71fcdd6a25a66b34d
Gabriela Escalona <gescalona2022@udec.cl>
2024-10-16 17:18:20 -0300

    feat: agrego clase InformeDeReunion

--- src/main/java/Asistencia.java
+++ src/main/java/Asistencia.java
@@ -1,6 +1,17 @@
 public class Asistencia {
+    /**
+     * Asistencia sirve para representar que un empleado asistió.
+     */
+    private Empleado emp;
 
-    public Asistencia(){
-
+    public Asistencia(Empleado emp){
+        this.emp= emp;
+    }
+    /**
+     * El siguiente método devuelve el empleado que ha asistido.
+     * @return empleado Empleado que asistió.
+     */
+    public Empleado getEmpleado(){
+        return emp;
     }
         }
