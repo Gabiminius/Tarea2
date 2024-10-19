6ad6be81b143927a8d15819865bc80efbf1f47ef
arenisca <vseron2000@gmail.com>
2024-10-19 16:59:21 -0300

    Implementación excepción cuando se quiere iniciar reunión en curso

---
+++ src/main/java/IniciarReunionencursoException.java
@@ -0,0 +1,8 @@
+public class IniciarReunionencursoException extends Exception {
+    /**
+     * @param message Mensaje que se mostrará en la excepción.
+     */
+    public IniciarReunionencursoException(String message) {
+        super(message);
+    }
+}
