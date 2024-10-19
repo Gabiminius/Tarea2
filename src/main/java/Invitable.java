f2c2f35463c6d208fdcbc1dc69a8acfb70cef2fa
arenisca <vseron2000@gmail.com>
2024-10-19 16:17:53 -0300

    Invitable.java

--- src/main/java/Invitable.java
+++ src/main/java/Invitable.java
@@ -1,12 +1,11 @@
 
-
 import java.time.Instant;
 
 public interface Invitable {
 
     public void invitar(Reunion reunion);
-
-
-
+    /* 
+     * Esta interfaz sirve para invitar cuando se implemente esta sección 
+     */
 
 }
