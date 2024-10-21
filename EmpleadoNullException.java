2c7809c47a3f8f451fcb1411404b83591eed293b
arenisca <vseron2000@gmail.com>
2024-10-20 23:01:19 -0300

    Caso excepción de empleado nulo

---
+++ EmpleadoNullException.java
@@ -0,0 +1,11 @@
+/**
+ * Si el empleado no es válido, se tendrá esta excepción.
+ */
+public class EmpleadoNullException extends Exception{
+    /**
+     * @param message es el mensaje que mostrará esta excepción
+     */
+    public EmpleadoNullException(String message) {
+        super(message);
+    }
+}
